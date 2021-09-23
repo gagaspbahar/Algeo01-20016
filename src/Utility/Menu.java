83c4cc463337ab1970397acd399d920a1542e65b
gagaspbahar <gagaspbahar@gmail.com>
2021-09-24 04:18:48 +0700

    Cofactor; New constructors

--- src/Utility/Menu.java
+++ src/Utility/Menu.java
@@ -14,43 +14,66 @@ public class Menu {
         System.out.println("4. Interpolasi polinom");
         System.out.println("5. Regresi linier berganda");
         System.out.println("6. Keluar");
+        System.out.println();
         int choice = sc.nextInt();
 
         switch(choice){
             case 1:
-                System.out.println("1. Metode eliminasi Gauss");
-                System.out.println("2. Metode eliminasi Gauss-Jordan");
-                System.out.println("3. Metode matriks balikan");
-                System.out.println("4. Kaidah Cramer");
+                SPLMenu();
             case 2:
                 determinantMenu();
-            
             case 3:
-                System.out.println("1. Metode eliminasi Gauss");
-                System.out.println("2. Metode matriks adjoin");
+                inverseMenu();
             case 4:
+                interpolateMenu();
             case 5:
+                regressionMenu();
             case 6:
                 exit();
         }
         sc.close();
     }
+
+    public static void SPLMenu(){
+        System.out.println("1. Metode eliminasi Gauss");
+        System.out.println("2. Metode eliminasi Gauss-Jordan");
+        System.out.println("3. Metode matriks balikan");
+        System.out.println("4. Kaidah Cramer");
+        System.out.println();
+    }
+
     public static void determinantMenu(){
-        
         System.out.println("1. Metode reduksi baris (Gauss)");
         System.out.println("2. Ekspansi kofaktor");
+        System.out.println();
         int choice = sc.nextInt();
         Matrix m;
         double det;
+        m = MatrixInput.Input();
         switch (choice){
             case 1:
-                m = MatrixInput.Input();
                 det = m.determinantByOBE();
                 System.out.println("Determinan dari matriks adalah " + det);     
                 prompt(); 
             case 2:
-                
+                det = m.determinantCofactor();
+                System.out.println("Determinan dari matriks adalah " + det);     
+                prompt();
         }
+    }
+
+    public static void inverseMenu(){
+        System.out.println("1. Metode eliminasi Gauss");
+        System.out.println("2. Metode matriks adjoin");
+        System.out.println();
+    }
+
+    public static void interpolateMenu(){
+
+    }
+
+    public static void regressionMenu(){
+
     }
 
     // BENERIN PROMPTNYA !!! MSH NoSuchElementException
