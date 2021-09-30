4dc70d4cc8db39930f7a826d1bf48c95b19673e1
dhikaarta <16520019@std.stei.itb.ac.id>
2021-09-30 13:52:17 +0700

    added SPL catch error

--- src/Algorithm/SPL.java
+++ src/Algorithm/SPL.java
@@ -1,6 +1,7 @@
 package Algorithm;
 import Matrix.*;
 import Utility.*;
+import javax.swing.*;
 
 public class SPL {
     private boolean noSolutions;
@@ -33,6 +34,7 @@ public class SPL {
                 break;
             default:
                 System.out.println("Pilihan tidak valid. Mengembalikan ke menu awal.");
+                JOptionPane.showMessageDialog(null,"Terjadi error. Input mungkin tidak valid. Mengulangi menu. " ,"Error!", JOptionPane.ERROR_MESSAGE);
                 Menu.SPLMenu();
         }
         this.solution = ans;
